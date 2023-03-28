48c827e6a2a540876f4134e1281ee79bc1a843f6
Dat Tran <dat.tran@ghielectronics.com>
2023-03-28 15:54:19 -0400

    to 0.0.0.5000

--- python/setup.py
+++ python/setup.py
@@ -2,12 +2,12 @@ import setuptools
 
 setuptools.setup(
     name = "Due",
-    version = "0.0.1",
+    version = "0.0.0.5000",
     author = "GHI Electronics",
     author_email = "support@ghielectronics.com",
     description = "GHI Electronics Due Python library.",
     license = "MIT",
-    url = "https://www.duelang.com/",
+    url = "https://www.duelink.com/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Development Status :: Python 3",        
