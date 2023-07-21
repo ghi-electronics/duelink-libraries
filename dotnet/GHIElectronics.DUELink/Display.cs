d1663f391bac13a5aab03a1def2a09724ed8a8f1
Dat Tran <dat.tran@ghielectronics.com>
2023-07-21 16:57:44 -0400

    add DrawBuffer supports 1,4,8,16 bits

--- dotnet/GHIElectronics.DUELink/Display.cs
+++ dotnet/GHIElectronics.DUELink/Display.cs
@@ -124,7 +124,7 @@ namespace GHIElectronics.DUELink {
             public bool DrawImage(uint[] img, int x, int y, int transform) => this.DrawImageScale(img, x, y, 1, 1, transform);
             public bool DrawImageScale(uint[] img, int x, int y, int scaleWidth, int scaleHeight, int transform) {
 
-                if ( img == null) {
+                if (img == null) {
                     throw new ArgumentNullException("Data null.");
                 }
 
@@ -142,7 +142,7 @@ namespace GHIElectronics.DUELink {
                 var res = this.serialPort.ReadRespone();
 
                 for (var i = 0; i < img.Length; i++) {
-                    cmd = string.Format("a[{0}] = {1}", (i) , img[i]);
+                    cmd = string.Format("a[{0}] = {1}", (i), img[i]);
 
                     this.serialPort.WriteCommand(cmd);
 
@@ -160,7 +160,7 @@ namespace GHIElectronics.DUELink {
 
                     res = this.serialPort.ReadRespone();
                 }
-                       
+
 
                 cmd = string.Format("dim a[0]"); // free array
 
@@ -188,8 +188,8 @@ namespace GHIElectronics.DUELink {
 
             //}
 
-            private bool Stream(byte[] data) {
-                var cmd = string.Format("lcdstream()");
+            private bool Stream(byte[] data, int color_depth) {
+                var cmd = string.Format("lcdstream({0})", color_depth);
 
                 this.serialPort.WriteCommand(cmd);
 
@@ -209,37 +209,114 @@ namespace GHIElectronics.DUELink {
             }
 
             public bool DrawBuffer(uint[] color) {
-                const int WIDTH = 128;
-                const int HEIGHT = 64;
+                //TODO: Need to be rewrite with SPI supported
 
-                if (color == null) {
-                    throw new Exception("color can not be null.");
+                return false;                
+
+            }
+
+            // This function for testing firmware that support 1,4,8,16bit
+            public void DrawBuffer(uint[] bitmap, int color_depth) {
+                if (bitmap == null) {
+                    throw new Exception("Bitmap array is null");
                 }
+                // todo: get width from config
+                var width = 128;
+                var height = 64;
 
-                int offset = 0;
-                int length = color.Length;
+                var buffer_size = 0;
+                var i = 0;
+                var buffer = Array.Empty<byte>();
+            
 
+                switch (color_depth) {
+                    case 1:
+                        buffer_size = width * height / 8;
 
-                var data = new byte[WIDTH * HEIGHT / 8]; // always send all screen, less than sreen size, fill by zero
-                var i = offset;
+                        buffer = new byte[buffer_size];
 
-                for (int y = 0; y < HEIGHT; y++) {
-                    for (int x = 0; x < WIDTH; x++) {
+                        for (int y = 0; y < height; y++) {
+                            for (int x = 0; x < width; x++) {
 
-                        var index = (y >> 3) * WIDTH + x;
+                                var index = (y >> 3) * width + x;
 
-                        if (i < offset + length) {
-                            if ((color[i] & 0x00FFFFFF) != 0) { // no alpha
-                                data[index] |= (byte)(1 << (y & 7));
+                                
+                                if ((bitmap[i] & 0x00FFFFFF) != 0) { // no alpha
+                                    buffer[index] |= (byte)(1 << (y & 7));
+                                }
+                                else {
+                                    buffer[index] &= (byte)(~(1 << (y & 7)));
+                                }
+                                i++;
+                                
                             }
-                            else {
-                                data[index] &= (byte)(~(1 << (y & 7)));
-                            }
-                            i++;
                         }
-                    }
+
+                        break;
+
+                    case 4:
+                        buffer_size = width * height / 2;
+
+                        buffer = new byte[buffer_size];
+
+                        for (i = 0; i < buffer.Length; i++) {
+                            buffer[i] = (byte)((bitmap[i * 2] << 4) | bitmap[i * 2 + 1]);
+                        }
+
+                        break;
+
+                    case 8:
+                        buffer_size = width * height;
+
+                        buffer = new byte[buffer_size];
+
+
+                        for (i = 0; i < buffer.Length; i++) {
+                            var clr = bitmap[i];
+
+                            var red = (clr >> 16) & 0xFF;
+                            var green = (clr >> 8) & 0xFF;
+                            var blue = (clr >> 0) & 0xFF;
+
+                            red = red >> 5;
+                            green = green >> 5;
+                            blue = blue >> 6;
+
+                            buffer[i] = (byte)((red << 5) | green << 3 | blue);
+                        }
+
+                        break;
+
+                    default:
+                        buffer_size = width * height * 2;
+
+                        buffer = new byte[buffer_size];
+
+
+                        i = 0;
+                        for (var y = 0; y < height; y++) {
+                            for (var x = 0; x < width; x++) {
+
+                                var index = (y * width + x) * 2;
+                                var clr = bitmap[i];
+
+                                buffer[index + 0] = (byte)(((clr & 0b0000_0000_0000_0000_0001_1100_0000_0000) >> 5) | ((clr & 0b0000_0000_0000_0000_0000_0000_1111_1000) >> 3));
+                                buffer[index + 1] = (byte)(((clr & 0b0000_0000_1111_1000_0000_0000_0000_0000) >> 16) | ((clr & 0b0000_0000_0000_0000_1110_0000_0000_0000) >> 13));
+                                i++;
+
+                            }
+                        }
+
+                        
+
+                        break;
                 }
-                return Stream(data);
+
+                if (buffer != null) {
+
+                    this.Stream(buffer, color_depth);
+                }
+
 
             }
 
@@ -267,8 +344,9 @@ namespace GHIElectronics.DUELink {
 
 
             }
-            public bool Configuration(int slaveAddress) {
-                var cmd = string.Format("lcdconfig(0,{0})", slaveAddress);
+
+            public bool Configuration(int target, uint param) {
+                var cmd = string.Format("lcdconfig({0},{1})", target, param);
 
                 this.serialPort.WriteCommand(cmd);
 
