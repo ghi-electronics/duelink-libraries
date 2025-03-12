3fe740304565de480776d829dabd5f032ccafbf7
Dat Tran <dat.tran@ghielectronics.com>
2025-03-12 17:10:01 -0400

    frequency from 0 to 1

--- python/DUELink/Frequency.py
+++ python/DUELink/Frequency.py
@@ -8,10 +8,9 @@ class FrequencyController:
         if frequency < self.MinFrequency or frequency > self.MaxFrequency:
             raise ValueError("Frequency must be in range 15Hz..10000000Hz")
 
-        if duration_ms > 99999999:
-            raise ValueError("duration_ms must be in range 0..99999999")
 
-        if dutycyle < 0 or dutycyle > 100:
+
+        if dutycyle < 0 or dutycyle > 0.1:
             raise ValueError("dutycyle must be in range 0..100")
         
         if pin not in self.serialPort.DeviceConfig.PWMPins:
