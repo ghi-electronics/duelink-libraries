bc2ea6b70e9f14e73e7ed29048cce0e03f879ee8
Dat Tran <dat.tran@ghielectronics.com>
2025-03-14 17:31:20 -0400

    to v1.2.0

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name = "DUELink",
-    version = "1.1.6",
+    version = "1.2.0",
     author = "GHI Electronics",
     author_email = "support@ghielectronics.com",
     license='MIT', 
