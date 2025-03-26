cab54d25ec7b45ec883e525df29c531e725f29c5
Dat Tran <dat.tran@ghielectronics.com>
2025-03-26 12:48:27 -0400

    update example

--- duelink/micropython/example.py
+++ duelink/micropython/example.py
@@ -3,25 +3,20 @@ import machine
 import duelink
 
 from duelink import transport
-
-
 from machine import Pin, Timer
-
-
-
-
 
 sclPIN = machine.Pin(23)
 sdaPIN = machine.Pin(22)
 
-
 i2c = transport.I2CTransportController(sda=sdaPIN, scl=sclPIN)
 due = duelink.DUELinkController(i2c)
 
+print(due.System.Info(1))
+
 while True:
-    due.Digital.Write(0, 0)
+    due.Led.Set(1,0,0)
     time.sleep(0.5)
-    due.Digital.Write(0, 1)
+    due.Led.Set(0,1,0)
     time.sleep(0.5)
 
 
