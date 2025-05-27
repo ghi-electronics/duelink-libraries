0631ee74ee2180c2cf53b785fbd4e62e972309fa
taylorza <chris_taylor_za@hotmail.com>
2025-05-26 20:36:18 -0400

    MicroPython Libraries

--- duelink/micropython/duelink/__init__.py
+++ duelink/micropython/duelink/__init__.py
@@ -1,8 +1,29 @@
-from duelink.transport import I2CTransportController
 from duelink.duelinklib import DUELinkController
-from duelink.digital import DigitalController
-from duelink.led import LedController
-from duelink.analog import AnalogController
-from duelink.button import ButtonController
-from duelink.system import SystemController
 
+def build_bytearray(data, offset=0, count=-1):
+    if count == -1:
+        count = len(data)-offset
+    arr = "["
+    i=offset;
+    while count > 0:
+        if (i>offset):
+            arr=arr+","
+        arr=arr+str(data[i])
+        i=i+1
+        count=count-1
+    arr=arr+"]"
+    return arr
+
+def build_floatarray(data, offset=0, count=-1):
+    if count == -1:
+        count = len(data)-offset
+    arr = "{"
+    i=offset;
+    while count > 0:
+        if (i>offset):
+            arr=arr+","
+        arr=arr+str(data[i])
+        i=i+1
+        count=count-1
+    arr=arr+"}"
+    return arr
--- duelink/micropython/duelink/digital.py
+++ duelink/micropython/duelink/digital.py
@@ -1,11 +1,11 @@
-
+# CT: 05/26/2025 - Tested
 class DigitalController:    
 
     def __init__(self, transport):
         self.transport = transport
 
     def Read(self, pin, pull):
-        r, s = self.execute(f"dread({pin},{pull})")
+        r, s = self.transport.execute(f"dread({pin},{pull})")
         if s:
             return int(r, 10)        
 
