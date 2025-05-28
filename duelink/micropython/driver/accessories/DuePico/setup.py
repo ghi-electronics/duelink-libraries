cd83f7c4027d7bdb99b3ed47e8c9cabe21e325d0
Dat Tran <dat.tran@ghielectronics.com>
2025-05-28 15:57:45 -0400

    tweak setup.py

--- duelink/micropython/driver/accessories/DuePico/setup.py
+++ duelink/micropython/driver/accessories/DuePico/setup.py
@@ -2,16 +2,15 @@ import setuptools
 
 setuptools.setup(
     name = "mDLDuePico",
-    version = "0.0.3",
+    version = "0.0.4",
     author = "GHI Electronics",
     author_email = "support@ghielectronics.com",
     license='MIT', 
-    description = "GHI Electronics DUE MicroPython library.",
+    description = "GHI Electronics DuePico MicroPython library.",
     url = "https://www.duelink.com/",
     install_requires=["mDUELink"],
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: Implementation :: MicroPython",        
-    ],
-    readme = "README.md"
+        "Programming Language :: Python :: Implementation :: MicroPython"
+    ]    
 )
--- duelink/micropython/standard/setup.py
+++ duelink/micropython/standard/setup.py
@@ -2,15 +2,14 @@ import setuptools
 
 setuptools.setup(
     name = "mDUELink",
-    version = "0.0.2",
+    version = "0.0.3",
     author = "GHI Electronics",
     author_email = "support@ghielectronics.com",
     license='MIT', 
-    description = "GHI Electronics DUE MicroPython library.",
+    description = "GHI Electronics DUELink MicroPython library.",
     url = "https://www.duelink.com/",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: Implementation :: MicroPython",        
-    ],
-    readme = "README.md"
+        "Programming Language :: Python :: Implementation :: MicroPython"
+    ]    
 )
