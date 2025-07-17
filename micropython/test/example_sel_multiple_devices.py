04ae30b955a098137a9992940eab4f35155787b0
Dat Tran <dat.tran@ghielectronics.com>
2025-07-17 09:25:36 -0400

    update test

--- micropython/test/example_sel_multiple_devices.py
+++ micropython/test/example_sel_multiple_devices.py
@@ -1,32 +1,32 @@
 import time
 import duelink  # standard lib
-import genericmodule  # standard lib
+import genericmodule  # template lib
 
 from duelink import transport
 from genericmodule import GenericModuleController
 
 due = duelink.DUELinkController(transport.UartTransportController(0))
-gen = GenericModuleController()
+gen = GenericModuleController(due)
 
-# Play beep at pin 11 on DuePico
-due.Sound.Beep(11, 1000, 500)
 
-while (1):
+while (1):    
     due.Engine.Select(1)
-    gen.StatLed(100,100, 10)
+    due.Sound.Beep(11, 1000, 500)
+    gen.StatLed(100,100, 1)
     time.sleep(3)
     
     due.Engine.Select(2)
-    gen.StatLed(100,100, 10)
+    gen.StatLed(100,100, 2)
     time.sleep(3)
     
     due.Engine.Select(3)
-    gen.StatLed(100,100, 10)
+    gen.StatLed(100,100, 3)
     time.sleep(3)
     
     due.Engine.Select(4)
-    gen.StatLed(100,100, 10)
+    gen.StatLed(100,100, 4)
     time.sleep(3)
+    
     
 
 
