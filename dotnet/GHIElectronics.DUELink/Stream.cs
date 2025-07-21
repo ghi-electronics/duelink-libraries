56b7a0844e805c2a197f5fcf391ad32199d2bf44
Dat Tran <dat.tran@ghielectronics.com>
2025-07-21 13:09:07 -0400

    rewrite stream when readresponse changed

--- dotnet/GHIElectronics.DUELink/Stream.cs
+++ dotnet/GHIElectronics.DUELink/Stream.cs
@@ -25,73 +25,150 @@ namespace GHIElectronics.DUELink {
 
                 return res.success;
             }
-            public bool WriteBytes(string array, byte[] data, int count) {
+            public int WriteBytes(string array, byte[] data, int count) {
 
                 var cmd = string.Format("strmwr({0}, {1})", array, count);
 
                 this.serialPort.WriteCommand(cmd);
 
-                var res = this.serialPort.ReadResponse();
-
-                if (res.success) {
-                    this.serialPort.WriteRawData(data, 0, data.Length);
+                // wait for prompt &
+                while (this.serialPort.BytesToRead() == 0) {
+                    Thread.Sleep(1);
                 }
 
-                return res.success;
-            }
+                var prompt = this.serialPort.ReadByte();
 
-            public bool WriteFloats(string array, float[] data, int count) {
+                if (prompt != '&') {
+                    throw new Exception("Wrong response package");
+                }
 
-                var cmd = string.Format("strmwr({0}, {1})", array, count);
+                // ready write data
+                this.serialPort.WriteRawData(data, 0, data.Length);
 
-                this.serialPort.WriteCommand(cmd);
+                // read x\r\n> (asio(1) not return this)
+                var ret = this.serialPort.ReadResponse();
 
-                var res = this.serialPort.ReadResponse();
-
-                if (res.success) {
-                    for (var i = 0; i < count; i++) {
-                        var b = BitConverter.GetBytes(data[i]);
-                        this.serialPort.WriteRawData(b, 0, b.Length);
+                if (ret.success) {
+                    try {
+                        var written = int.Parse(ret.response);
+                        return written;
                     }
+                    catch {
 
-                   
+                    }
                 }
-
-                return res.success;
+                return 0;
             }
 
-            public bool ReadBytes(string array, byte[] data, int count) {
+            public int WriteFloats(string array, float[] data, int count) {
+
+                var cmd = string.Format("strmwr({0}, {1})", array, count);
+
+                this.serialPort.WriteCommand(cmd);
+
+                // wait for prompt &
+                while (this.serialPort.BytesToRead() == 0) {
+                    Thread.Sleep(1);
+                }
+
+                var prompt = this.serialPort.ReadByte();
+
+                if (prompt != '&') {
+                    throw new Exception("Wrong response package");
+                }
+
+                // ready write data
+                for (var i = 0; i < count; i++) {
+                    var b = BitConverter.GetBytes(data[i]);
+                    this.serialPort.WriteRawData(b, 0, b.Length);
+                }
+
+                // read x\r\n> (asio(1) not return this)
+                var ret = this.serialPort.ReadResponse();
+
+                if (ret.success) {
+                    try {
+                        var written = int.Parse(ret.response);
+                        return written;
+                    }
+                    catch {
+
+                    }
+                }
+                return 0;
+            }
+
+            public int ReadBytes(string array, byte[] data, int count) {
 
                 var cmd = string.Format("strmrd({0}, {1})", array, count);
 
                 this.serialPort.WriteCommand(cmd);
 
-                var res = this.serialPort.ReadResponse();
-
-                if (res.success) {
-                    this.serialPort.ReadRawData(data, 0, data.Length);
+                // wait for prompt &
+                while (this.serialPort.BytesToRead() == 0) {
+                    Thread.Sleep(1);
                 }
 
-                return true;
+                var prompt = this.serialPort.ReadByte();
+
+                if (prompt != '&') {
+                    throw new Exception("Wrong response package");
+                }
+
+                // ready to read data
+                this.serialPort.ReadRawData(data, 0, data.Length);
+
+                // read x\r\n> (asio(1) not return this)
+                var ret = this.serialPort.ReadResponse();
+
+                if (ret.success) {
+                    try {
+                        var read = int.Parse(ret.response);
+                        return read;
+                    }
+                    catch {
+
+                    }
+                }
+                return 0;
             }
-            public bool ReadFloats(string array, float[] data, int count) {
+            public int ReadFloats(string array, float[] data, int count) {
                 var data_bytes = new byte[count * 4];
 
                 var cmd = string.Format("strmrd({0}, {1})", array, count);
 
                 this.serialPort.WriteCommand(cmd);
 
-                var res = this.serialPort.ReadResponse();
-
-                if (res.success) {
-                    this.serialPort.ReadRawData(data_bytes, 0, data_bytes.Length);
+                // wait for prompt &
+                while (this.serialPort.BytesToRead() == 0) {
+                    Thread.Sleep(1);
                 }
+
+                var prompt = this.serialPort.ReadByte();
+
+                if (prompt != '&') {
+                    throw new Exception("Wrong response package");
+                }
+
+                this.serialPort.ReadRawData(data_bytes, 0, data_bytes.Length);
 
                 for (var i = 0; i < data_bytes.Length; i += 4) {
-                    data[i/4] = BitConverter.ToSingle(data_bytes, i);
+                    data[i / 4] = BitConverter.ToSingle(data_bytes, i);
                 }
 
-                return true;
+                // read x\r\n> (asio(1) not return this)
+                var ret = this.serialPort.ReadResponse();
+
+                if (ret.success) {
+                    try {
+                        var written = int.Parse(ret.response);
+                        return written;
+                    }
+                    catch {
+
+                    }
+                }
+                return 0;
             }
         }
     }
