58ac5b0e47e609668d24e846fb0f0c64ab1464b3
Dat Tran <dat.tran@ghielectronics.com>
2025-08-08 16:22:31 -0400

    remove debug

--- javascript/src/debug/index.js
+++
@@ -1,9 +0,0 @@
-import {SerialUSB} from './serialusb.js';
-import * as due from './duelink.js';
-import { Util } from "./util.js";
-//import { createCanvas, loadImage } from 'canvas';
-
-let duelink = new due.DUELinkController(new SerialUSB());
-await duelink.Connect();
-
-await duelink.Sound.Beep(7, 1000, 100);
