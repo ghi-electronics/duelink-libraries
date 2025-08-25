21205953b8b999ef702e4fd7ec83d51ba346e019
Dat Tran <dat.tran@ghielectronics.com>
2025-08-25 15:07:04 -0400

    Update analog and select device 1 as default

--- python/DUELink/Analog.py
+++ python/DUELink/Analog.py
@@ -5,12 +5,31 @@ class AnalogController:
         self.serialPort = serialPort
         self.Fixed_Frequency = 50
 
-    def Read(self, pin):
+    def VoltRead(self, pin):
 
         if pin not in self.serialPort.DeviceConfig.AnalogPins:
             raise ValueError("Invalid pin. Enter a valid analog pin.")
 
         cmd = "vread({0})".format(pin)
+
+        self.serialPort.WriteCommand(cmd)
+
+        ret = self.serialPort.ReadResponse()
+
+        if ret.success:
+            try:
+                return float(ret.response)
+            except:
+                pass
+
+        return -1
+    
+    def Read(self, pin):
+
+        if pin not in self.serialPort.DeviceConfig.AnalogPins:
+            raise ValueError("Invalid pin. Enter a valid analog pin.")
+
+        cmd = "aread({0})".format(pin)
 
         self.serialPort.WriteCommand(cmd)
 
@@ -32,7 +51,7 @@ class AnalogController:
         if duty_cycle < 0 or duty_cycle > 1:
             raise ValueError('Duty cycle must be in the range 0..1')
 
-        cmd = f'pwrite({pin}, {duty_cycle})'
+        cmd = f'awrite({pin}, {duty_cycle})'
         self.serialPort.WriteCommand(cmd)
 
         ret = self.serialPort.ReadResponse()
