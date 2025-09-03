1b8a5c7947aaffeb63f3bc37b3d12337e08d436d
Dat Tran <dat.tran@ghielectronics.com>
2025-09-03 17:52:30 -0400

    add & fix stream floats

--- micropython/standard-library/duelink/stream.py
+++ micropython/standard-library/duelink/stream.py
@@ -131,8 +131,48 @@ class StreamController:
         for i in range (0, count):
             float_bytes = struct.pack('>f', dataWrite[i])
             #float_bytes_lsb = float_bytes[::-1]
-            float_bytes_lsb = bytes([float_bytes[i] for i in range(len(float_bytes)-1,0, -1)])
+            float_bytes_lsb = bytes([float_bytes[3],float_bytes[2],float_bytes[1],float_bytes[0]])
+              
             self.transport.WriteRawData(float_bytes_lsb,0, 4)        
+
+        # read x\r\n>
+        r,s = self.transport.ReadResponse()
+        if r == True:
+            try:
+                return int(s)
+            except:
+                return 0                
+        
+        return 0
+    
+    def ReadFloats(self, arr: str, dataRead: float):
+        if dataRead is None or dataRead == 0:
+            return 0
+        count = len(dataRead)
+
+        # declare b1 array
+        cmd = f"strmrd({arr},{count})"
+        self.transport.WriteCommand(cmd)
+
+        # wait for prompt &
+        prompt = 0
+        startms = time.ticks_ms()
+        while (time.ticks_ms() - startms < self.transport.ReadTimeout):            
+            time.sleep(0.001)
+            data = self.transport.ReadByte()
+            if data != None and data[0] == ord('&'):
+                prompt = data[0]
+                break
+
+        if prompt != ord('&'):
+            raise Exception("Invalid or no responses")
+        
+        # ready to read data
+        raw_bytes = bytearray(4)
+        for i in range (0, count):
+            self.transport.ReadRawData(raw_bytes,0, 4)
+            #raw_bytes_lsb = bytes([raw_bytes[3],raw_bytes[2],raw_bytes[1],raw_bytes[0]])
+            dataRead[i] = struct.unpack('f', raw_bytes)[0]        
 
         # read x\r\n>
         r,s = self.transport.ReadResponse()
