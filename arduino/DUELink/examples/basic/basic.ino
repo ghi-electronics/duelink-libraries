2dd48df7ca4c821e4f086e5e6871bf36ffb31bdf
Dat Tran <dat.tran@ghielectronics.com>
2025-09-08 11:11:00 -0400

    update basic test

--- arduino/DUELink/examples/basic/basic.ino
+++ arduino/DUELink/examples/basic/basic.ino
@@ -14,19 +14,25 @@ void setup() {
   Serial.begin(9600);
   Wire1.begin();
 
-
+  const float cfg[1]=  {0x3C};
+  float img[10]=  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
   duelink.Connect();
-  duelink.Engine.Select(1);
 
-  duelink.Graphics.Clear(1);
-  duelink.Graphics.Text("--DUELink basic--", 0, 10, 1);
-  duelink.Graphics.Text("Press button on", 0, 1, 21);
-  duelink.Graphics.Text("device 2 to play", 0, 1, 31);
-  duelink.Graphics.Text("sound on device 3", 0, 1, 41);
+  duelink.Graphics.Configuration(1, cfg, 1, 128, 64, 1); 
+
+  duelink.Graphics.Clear(0);
+  duelink.Graphics.Text("DUELink X1", 1, 0, 0);
+  duelink.Graphics.TextS("DUELink x2", 1, 0, 10, 2,2);
+  duelink.Graphics.Circle(1, 30, 30, 10);
+  duelink.Graphics.Line(1, 0, 40, 50, 40);
+  duelink.Graphics.Fill(1, 0, 45, 20, 5);
+  duelink.Graphics.DrawImage(img, sizeof(img)/sizeof(float), 0, 55, 5, 2, 0);
+  duelink.Graphics.Rect(1, 0, 60, 20, 4);
+
   duelink.Graphics.Show();
 
   duelink.Engine.Select(2);  
-  duelink.Button.Enable(1, 1, 1);
+  duelink.Button.Enable(1, 0);
   
 }
 
@@ -34,7 +40,7 @@ void loop() {
   delay(100);
 
   duelink.Engine.Select(2);   
-  if (duelink.Button.Up(1) == 1) {
+  if (duelink.Button.Down(1) == 1) {
     duelink.Engine.Select(3);  
     duelink.Frequency.Write(7, 1000, 50, 0.5);
      
