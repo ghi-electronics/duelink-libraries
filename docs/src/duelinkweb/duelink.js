733e256a3784272a73ca6bf223128bb54923ab6a
Dat Tran <dat.tran@ghielectronics.com>
2025-11-19 12:18:41 -0500

    update util name

--- docs/src/duelinkweb/duelink.js
+++ docs/src/duelinkweb/duelink.js
@@ -1,5 +1,5 @@
 export { SerialInterface, DUELinkController}
-import { Util } from '../util/dlutil.js';
+import { Util } from '../util/duelinkutil.js';
 
 class SerialInterface {
     isReady = false;
