fe5066a39e2bbb54f02fcb414779d86248374fab
Dat Tran <dat.tran@ghielectronics.com>
2026-02-18 11:32:27 -0500

    to v0.11

--- micropython/standard-library/setup.py
+++ micropython/standard-library/setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name = "duelink-stdlib-mp",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "GHI Electronics",
     author_email = "support@ghielectronics.com",
     license='MIT', 
